ec0fca0e0330c75549bbc24bc115cad51e00b6ba
alexRai98 <raikcuerda19@gmail.com>
2020-09-29 00:21:55 -0500

    implement endpoints post and get /users/

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @user = User.all
+    if @user
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_name ,:email, :name, :url_photo)
+  end
+end
